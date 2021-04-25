ec797ede699499f0aecb18e5d3d21cbae9bd64ef
paragrapharamus <vg.apostol@yahoo.com>
2021-04-25 20:56:00 +0100

    Added SLURM script

---
+++ slurnrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=va4317
+export PATH=/vol/bitbucket/${USER}/miniconda3/bin/:$PATH
+source activate
+conda activate msdp
+source /vol/cuda/11.1.0-cudnn8.0.4.30/setup.sh
+TERM=vt100 # or TERM=xterm
+
+/usr/bin/nvidia-smi
+uptime
+
+nohup python -u main.py > out/exp.log 2>&1
+
