1286bc19e2eb7b6918c0a7839be8728db79d2570
paragrapharamus <vg.apostol@yahoo.com>
2021-05-31 17:54:25 +0100

    Moved plotting code to utils

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from typing import Type
 
-import os
 import warnings
 
-import numpy as np
 import pytorch_lightning as pl
 import torch.optim
-from matplotlib.ticker import NullFormatter
 from torch import nn
 from matplotlib import pyplot as plt
-from shutil import move, rmtree
 
 from attacks import (model_extraction,
                      membership_inference_black_box,
@@ -23,8 +19,9 @@ from dp_stages import Stages
 from fl.aggregator import Aggregator
 from fl.fl import FLEnvironment
 from log import Logger
-from models import Cifar10Net, MnistCNNNet, MnistFCNet, SqueezeNetDR, MnistClassifierCNN, Cifar10Classifier, Cifar10ResNet
+from models import Cifar10Net, MnistCNNNet, MnistFCNet, SqueezeNetDR, MnistClassifierCNN, Cifar10Classifier
 from msdp import MSPDTrainer
+from utils import *
 
 
 def _set_seed(seed=42):
@@ -197,8 +194,8 @@ def _train_fl_and_attack(args, model_cls, dataset_name):
 
   train_dataset, test_dataset = get_dataset(dataset_name, False)
 
-  args.experiment_id = _get_next_available_dir(f'{args.save_dir}/lightning_logs', 'experiment', False, False)
-  args.save_model_path = _get_next_available_dir(args.save_dir, 'checkpoints', True, True)
+  args.experiment_id = get_next_available_dir(f'{args.save_dir}/lightning_logs', 'experiment', False, False)
+  args.save_model_path = get_next_available_dir(args.save_dir, 'checkpoints', True, True)
 
   fl_simulator = FLEnvironment(model_class=model_cls,
                                train_dataset=train_dataset,
@@ -462,12 +459,12 @@ def msdpfl_on_mnist():
   args.name = "MSDPFL on CIFAR10"
   args.num_rounds = 10
   args.epochs = 5
-  args.eps1 = 10
-  args.noise_multiplier = 0.7
-  args.max_grad_norm = 5
-  args.eps3 = 10
-  args.max_weight_norm = 20
-  args.max_weight_norm_aggregated = 20
+  args.eps1 = 5
+  args.noise_multiplier = 1.5
+  args.max_grad_norm = 2
+  args.eps3 = 5
+  args.max_weight_norm = 15
+  args.max_weight_norm_aggregated = 15
   args.num_clients = 10
   args.alpha = 10
 
@@ -481,12 +478,12 @@ def fl_opacus_on_mnist():
   args = ExperimentConfig()
   args.name = "FL Opacus on MNIST"
   args.num_rounds = 10
-  args.epochs = 2
+  args.epochs = 5
   args.stage1 = False
   args.stage2 = True
   args.stage3 = False
   args.stage4 = False
-  args.noise_multiplier = 2
+  args.noise_multiplier = 2.5
   args.max_grad_norm = 2
   args.num_clients = 10
   args.alpha = 10
@@ -678,27 +675,12 @@ def opacus_fl_training_on_dr():
 
 def run_experiments():
   experiments = [
-    msdpfl_on_cifar10,
-    fl_opacus_on_cifar10,
+    msdpfl_on_mnist,
+    fl_opacus_on_mnist,
   ]
 
   for exp in experiments:
     exp()
-
-
-def _get_next_available_dir(root, dir_name, absolute_path=True, create=True):
-  checkpoint_dir_base = os.path.join(root, dir_name)
-  dir_id = 1
-  checkpoint_dir = f"{checkpoint_dir_base}_{dir_id}"
-  while os.path.exists(checkpoint_dir):
-    dir_id += 1
-    checkpoint_dir = f"{checkpoint_dir_base}_{dir_id}"
-  if create:
-    os.mkdir(checkpoint_dir)
-  if absolute_path:
-    return checkpoint_dir
-  else:
-    return f"{dir_name}_{dir_id}"
 
 
 def attack_test():
@@ -713,140 +695,9 @@ def attack_test():
   args.model_inversion = True
   # args.knockoffnet_extraction = True
 
-  args.name = 'msdpfl_cifar'
+  args.name = 'msdpfl_cifar-low'
   attack_model(args, device, 'cifar10', architecture=model_cls,
-               checkpoint_path='outFL/CIFAR10/msdpfl/final.ckpt')
-
-
-def _plot(data_dict, x_label, y_label, title):
-  fig, ax = plt.subplots()  # (figsize=(10,10))
-
-  if 'x_ticks' in data_dict:
-    x_values = data_dict.pop('x_ticks')
-    if len(x_values) > 20:
-      x_values = None  # too crowded to read on the figure
-  else:
-    x_values = None
-
-  max_x_range_len = 0
-  for name, data in data_dict.items():
-    if x_values is not None:
-      ax.plot(list(range(len(x_values))), data, label=name)
-    else:
-      ax.plot(data, label=name)
-
-  if x_values is not None:
-    plt.xticks(list(range(len(x_values))), x_values)
-
-  ax.legend()
-  plt.xlabel(x_label)
-  plt.ylabel(y_label)
-  plt.title(title)
-  plt.show()
-  return fig
-
-
-def load_and_plot_privacy_param_variation():
-  eps1 = {'name': 'eps_1', 'fp': './eps1.npy', }
-  noise_multiplier = {'name': 'noise_multiplier', 'fp': './noise_multiplier.npy'}
-  eps3 = {'name': 'eps_3', 'fp': './eps3.npy'}
-
-  files = [eps3]
-
-  curve_names = ['Test accuracy', 'MEA fidelity', 'MIA accuracy']
-
-  for data_file in files:
-    data = dict()
-    with open(data_file['fp'], 'rb') as f:
-      data['x_ticks'] = np.load(f)
-      for curve in curve_names:
-        data[curve] = np.load(f)
-      # data['x_ticks'] = np.array(data_file['rng'])
-    _plot(data, data_file['name'], 'Privacy and Utility', 'Small CNN on Cifar10')
-
-
-def load_and_plot_learning_curves():
-  def fetch(fs, metric_name):
-    metric_data = dict()
-    for f in fs:
-      metric_data[f['name']] = f[metric_name]
-    return metric_data
-
-  metrics = ['train_loss', 'train_acc', 'val_acc']
-
-  msdp = {'name': 'MSDP $\epsilon<20.91$', 'fp': "out_centralMSDP/CIFAR10/msdp/MSDPTrainer_0_plot_stats.npy"}
-  opacus = {'name': 'Opacus $\epsilon=20.91$', 'fp': "out_centralMSDP/CIFAR10/opacus/MSDPTrainer_0_plot_stats.npy"}
-  non_p = {'name': 'Non-Private', 'fp': "out_centralMSDP/CIFAR10/np/MSDPTrainer_0_plot_stats.npy"}
-
-  msdp2 = {'name': 'MSDP $\epsilon<0.99$', 'fp': "out_centralMSDP/CIFAR10/new/msdp/MSDPTrainer_0_plot_stats.npy"}
-  opacus2 = {'name': 'Opacus $\epsilon=0.99$', 'fp': "out_centralMSDP/CIFAR10/new/opacus/MSDPTrainer_0_plot_stats.npy"}
-
-  title = 'CNN model on CIFAR-10'
-
-  files = [msdp, opacus, non_p, msdp2, opacus2]
-
-  for data_file in files:
-    data = dict()
-    with open(data_file['fp'], 'rb') as f:
-      for metric in metrics:
-        data[metric] = np.load(f)
-    data_file.update(**data)
-
-  for metric in metrics:
-    metric_data = fetch(files, metric)
-    f = _plot(metric_data, 'Epochs', metric, title)
-    # if metric == 'val_acc':
-    #   f.savefig(f"./val_acc.png", bbox_inches='tight')
-
-
-def load_and_plot_dr():
-  def fetch(fs, metric_name):
-    metric_data = dict()
-    for f in fs:
-      metric_data[f['name']] = f[metric_name]
-    return metric_data
-
-  def dr_plot(data_dict, x_label, y_label, title):
-    fig, ax = plt.subplots()  # (figsize=(10,10))
-    for name, data in data_dict.items():
-      ax.plot(data, label=name)
-    ax.legend()
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
-    plt.show()
-
-  metrics = {'centralised': ['train_loss', 'train_acc', 'val_acc'],
-             'fl': ['val_acc']
-             }
-
-  msdp = {'name': 'MSDP', 'fp': "out_centralMSDP/DR/msdp/MSDPTrainer_0_plot_stats.npy"}
-  msdpfl = {'name': 'MSDPFL', 'fp': "outFL/DR/msdpfl/stats.npy"}
-  opacus = {'name': 'Opacus', 'fp': "out_centralMSDP/DR/opacus/MSDPTrainer_0_plot_stats.npy"}
-  opacusfl = {'name': 'OpacusFL', 'fp': "outFL/DR/opacus_fl/stats.npy"}
-  non_p = {'name': 'Non-Private', 'fp': "out_centralMSDP/DR/np/MSDPTrainer_0_plot_stats.npy"}
-  non_pfl = {'name': 'Non-Private FL', 'fp': "outFL/DR/np_fl/stats.npy"}
-  title = 'FL training on DR'
-
-  central = [msdp, opacus, non_p]
-  fl = [msdpfl, opacusfl, non_pfl]
-  files = central + fl
-
-  for data_file in files:
-    data = dict()
-    if data_file in central:
-      metric_type = 'centralised'
-    else:
-      metric_type = 'fl'
-    with open(data_file['fp'], 'rb') as f:
-      for metric in metrics[metric_type]:
-        data[metric] = np.load(f)
-
-    data_file.update(**data)
-
-  for metric in ['val_acc']:
-    metric_data = fetch(files, metric)
-    dr_plot(metric_data, 'Epochs/ Rounds', metric, title)
+               checkpoint_path='outFL/CIFAR10/low_eps/msdpfl/final.ckpt')
 
 
 if __name__ == '__main__':
@@ -854,5 +705,5 @@ if __name__ == '__main__':
   # load_and_plot_privacy_param_variation()
   # load_and_plot_learning_curves()
   # load_and_plot_dr()
-  run_experiments()
-  # attack_test()
+  # run_experiments()
+  attack_test()
--- utils.py
+++ utils.py
@@ -1,5 +1,153 @@
+import os
+
+import numpy as np
+from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 
 
 def minibatch_loader(minibatch, minibatch_size, drop_last=True):
   return DataLoader(minibatch, batch_size=minibatch_size, drop_last=drop_last)
+
+
+def get_next_available_dir(root, dir_name, absolute_path=True, create=True):
+  checkpoint_dir_base = os.path.join(root, dir_name)
+  dir_id = 1
+  checkpoint_dir = f"{checkpoint_dir_base}_{dir_id}"
+  while os.path.exists(checkpoint_dir):
+    dir_id += 1
+    checkpoint_dir = f"{checkpoint_dir_base}_{dir_id}"
+  if create:
+    os.mkdir(checkpoint_dir)
+  if absolute_path:
+    return checkpoint_dir
+  else:
+    return f"{dir_name}_{dir_id}"
+
+
+def _plot(data_dict, x_label, y_label, title):
+  fig, ax = plt.subplots()  # (figsize=(10,10))
+
+  if 'x_ticks' in data_dict:
+    x_values = data_dict.pop('x_ticks')
+    if len(x_values) > 20:
+      x_values = None  # too crowded to read on the figure
+  else:
+    x_values = None
+
+  max_x_range_len = 0
+  for name, data in data_dict.items():
+    if x_values is not None:
+      ax.plot(list(range(len(x_values))), data, label=name)
+    else:
+      ax.plot(data, label=name)
+
+  if x_values is not None:
+    plt.xticks(list(range(len(x_values))), x_values)
+
+  ax.legend()
+  plt.xlabel(x_label)
+  plt.ylabel(y_label)
+  plt.title(title)
+  plt.show()
+  return fig
+
+
+def load_and_plot_privacy_param_variation():
+  eps1 = {'name': 'eps_1', 'fp': './eps1.npy', }
+  noise_multiplier = {'name': 'noise_multiplier', 'fp': './noise_multiplier.npy'}
+  eps3 = {'name': 'eps_3', 'fp': './eps3.npy'}
+
+  files = [eps1, noise_multiplier, eps3]
+
+  curve_names = ['Test accuracy', 'MEA fidelity', 'MIA accuracy']
+
+  for data_file in files:
+    data = dict()
+    with open(data_file['fp'], 'rb') as f:
+      data['x_ticks'] = np.load(f)
+      for curve in curve_names:
+        data[curve] = np.load(f)
+      # data['x_ticks'] = np.array(data_file['rng'])
+    _plot(data, data_file['name'], 'Privacy and Utility', 'Small CNN on Cifar10')
+
+
+def load_and_plot_learning_curves():
+  def fetch(fs, metric_name):
+    metric_data = dict()
+    for f in fs:
+      metric_data[f['name']] = f[metric_name]
+    return metric_data
+
+  metrics = ['val_acc']
+
+  msdp = {'name': 'MSDPFL', 'fp': "outFL/MNIST/low_eps/msdpfl/stats.npy"}
+  opacus = {'name': 'Opacus FL', 'fp': "outFL/MNIST/low_eps/opacusfl/stats.npy"}
+  non_p = {'name': 'Non-Private FL', 'fp': "outFL/MNIST/npfl/stats.npy"}
+
+  title = 'Highly private FL training on MNIST'
+
+  files = [msdp, opacus, non_p]
+
+  for data_file in files:
+    data = dict()
+    with open(data_file['fp'], 'rb') as f:
+      for metric in metrics:
+        data[metric] = np.load(f)
+    data_file.update(**data)
+
+
+  for metric in metrics:
+    metric_data = fetch(files, metric)
+    f = _plot(metric_data, 'Epochs', metric, title)
+    if metric == 'val_acc':
+      f.savefig(f"./val_acc.png", bbox_inches='tight')
+
+
+def load_and_plot_dr():
+  def fetch(fs, metric_name):
+    metric_data = dict()
+    for f in fs:
+      metric_data[f['name']] = f[metric_name]
+    return metric_data
+
+  def dr_plot(data_dict, x_label, y_label, title):
+    fig, ax = plt.subplots()  # (figsize=(10,10))
+    for name, data in data_dict.items():
+      ax.plot(data, label=name)
+    ax.legend()
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.show()
+
+  metrics = {'centralised': ['train_loss', 'train_acc', 'val_acc'],
+             'fl': ['val_acc']
+             }
+
+  msdp = {'name': 'MSDP', 'fp': "out_centralMSDP/DR/msdp/MSDPTrainer_0_plot_stats.npy"}
+  msdpfl = {'name': 'MSDPFL', 'fp': "outFL/DR/msdpfl/stats.npy"}
+  opacus = {'name': 'Opacus', 'fp': "out_centralMSDP/DR/opacus/MSDPTrainer_0_plot_stats.npy"}
+  opacusfl = {'name': 'OpacusFL', 'fp': "outFL/DR/opacus_fl/stats.npy"}
+  non_p = {'name': 'Non-Private', 'fp': "out_centralMSDP/DR/np/MSDPTrainer_0_plot_stats.npy"}
+  non_pfl = {'name': 'Non-Private FL', 'fp': "outFL/DR/np_fl/stats.npy"}
+  title = 'FL training on DR'
+
+  central = [msdp, opacus, non_p]
+  fl = [msdpfl, opacusfl, non_pfl]
+  files = central + fl
+
+  for data_file in files:
+    data = dict()
+    if data_file in central:
+      metric_type = 'centralised'
+    else:
+      metric_type = 'fl'
+    with open(data_file['fp'], 'rb') as f:
+      for metric in metrics[metric_type]:
+        data[metric] = np.load(f)
+
+    data_file.update(**data)
+
+  for metric in ['val_acc']:
+    metric_data = fetch(files, metric)
+    dr_plot(metric_data, 'Epochs/ Rounds', metric, title)
